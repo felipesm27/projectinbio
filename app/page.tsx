49990ca02ffd2c0615af45eda6e06355506f8899
Felipe Mello <felipesmello@hotmail.com>
2025-04-11 00:40:02 -0300

    feat: finaliza component pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import Pricing from "./components/landing-page/Pricing";
+import VideoExplanation from "./components/landing-page/Video-Explanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Hero />
       <Header />
+      <VideoExplanation />
+      <Pricing />
     </div>
   );
 }
