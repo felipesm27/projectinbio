660599316ed4338e1363064295045c49fb4bf86b
Felipe Mello <felipesmello@hotmail.com>
2025-04-15 13:57:26 -0300

    feat: implement modal functionality

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -1,9 +1,12 @@
-import { getProfileData } from "../server/get-profile.data";
 import ProjectCard from "@/app/components/commons/Project-Card";
 import { TotalVisits } from "@/app/components/commons/Total-Visits";
 import UserCard from "@/app/components/commons/User-Card";
-import { PlusIcon } from "lucide-react";
+import { auth } from "@/app/lib/auth";
+import { getProfileData } from "../../server/get-profile-data";
+
 import Link from "next/link";
+import { notFound } from "next/navigation";
+import NewProject from "./new-project";
 
 export default async function ProfilePage({
   params,
@@ -13,21 +16,33 @@ export default async function ProfilePage({
   const { profileId } = await params;
 
   const profileData = await getProfileData(profileId);
-  console.log("Profile Data:", profileData);
+
+  if (!profileData) return notFound();
+
+  // TODO: get projects
+
+  const session = await auth();
+
+  const isOwner = profileData.userId === session?.user?.id;
+
+  // TODO: Adicionar page view
+
+  // Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
+
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
-      <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-secondary">
-        <span className="text-accent-green font-bold">
-          Você está usando a versão Trial.
-        </span>
+      <div className="fixed top-0 left-0 w-full flex justify-center items-center gap-1 py-2 bg-background-tertiary">
+        <span>Você está usando a versão trial.</span>
         <Link href={`/${profileId}/upgrade`}>
-          <button>Faça o upgrade agora!</button>
+          <button className="text-accent-green font-bold">
+            Faça o upgrade agora!
+          </button>
         </Link>
       </div>
       <div className="w-1/2 flex justify-center h-min">
         <UserCard />
       </div>
-      <div className="w-full flex justify-center content-start gap-4 flex-wrap overflow-auto">
+      <div className="w-full flex justify-center content-start gap-4 flex-wrap overflow-y-auto">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
@@ -35,12 +50,7 @@ export default async function ProfilePage({
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
-        <button className="w-[240px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border border-dashed border-border-secondary">
-          <span className="text-accent-pink">
-            <PlusIcon className="size-10" />
-          </span>
-          <span>Novo Projeto</span>
-        </button>
+        {isOwner && <NewProject profileId={profileId} />}
       </div>
       <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
         <TotalVisits />
